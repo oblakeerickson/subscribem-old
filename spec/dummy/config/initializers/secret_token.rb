d4662481b9061472918f53c89130635bf8ca815d
Blake Erickson <o.blakeerickson@gmail.com>
2014-01-27 07:37:00 -0700

    add sign-up form

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'd7a8fccb9c293b1e3f100c7edced6422bb9ded517afe939fac3cd652f6447c17f1cdb9448c29eaa86e328f4f002eaaf792432a7a051b0c6a6e367dbaca629cfe'
